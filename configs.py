fa7e45a2450a04b923603efa5f3e460557d38645
Abdelkarim CHAMLAL <abdelkarim.chamlal@gmail.com>
2022-01-22 02:43:40 +0100

    adding the configs file

---
+++ configs.py
@@ -0,0 +1,8 @@
+import random
+
+username = "username"   # instagram username
+password = "password"   # instagram password
+
+sleepDuration = random.randint(3600, 6 * 3600)  # new post every 1 two 6 hours
+picsFolderPath = "C://Users//UserName//Desktop//instaBot//pics//"   # path example
+allowedAspectRatios = [1.25, 1] # the current accepted image ratios in instagram
